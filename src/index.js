67c42dc577216adf9540ae1b8f3e0892f56372f2
Kelsey-Hunt <kels.hunt13@gmail.com>
2022-10-18 17:32:16 -0500

    completed all tasks - tests running successfully

--- src/index.js
+++ src/index.js
@@ -40,3 +40,55 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 };
 
 console.log('project wired!')
+
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach((element) => {
+  element.className = 'italic';
+})
+navLinks[0].textContent = 'Services';
+navLinks[1].textContent = 'Product';
+navLinks[2].textContent = 'Vision';
+navLinks[3].textContent = 'Features';
+navLinks[4].textContent = 'About';
+navLinks[5].textContent = 'Contact';
+
+const logoImg = document.querySelector('#logo-img');
+logoImg.setAttribute('src', "http://localhost:9000/img/logo.png");
+
+const h1 = document.querySelector('.cta-text h1');
+h1.textContent = 'DOM Is Awesome';
+
+const button = document.querySelector('button');
+button.textContent = 'Get Started';
+
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.setAttribute('src', "http://localhost:9000/img/cta.png");
+
+const texth4 = document.querySelectorAll('.text-content h4');
+texth4[0].textContent = 'Features';
+texth4[1].textContent = 'About';
+texth4[2].textContent = 'Services';
+texth4[3].textContent = 'Product';
+texth4[4].textContent = 'Vision';
+
+const textP = document.querySelectorAll('.text-content p');
+textP[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+textP[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+textP[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+textP[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+textP[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+const middleImg = document.querySelector('.middle-img');
+middleImg.setAttribute('src', "http://localhost:9000/img/accent.png")
+
+const contacth4 = document.querySelector('.contact h4');
+contacth4.textContent = 'Contact';
+
+const contactP = document.querySelectorAll('.contact p');
+contactP[0].textContent = '123 Way 456 Street Somewhere, USA';
+contactP[1].textContent = '1 (888) 888-8888';
+contactP[2].textContent = 'sales@greatidea.io';
+
+const footerLink = document.querySelector('footer a');
+footerLink.className = 'bold';
+footerLink.textContent = 'Copyright Great Idea! 2021';
